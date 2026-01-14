5384ff1673c92d867165bebb353fe9ceb9c4c531
Volodymyr <volodymyrshalata@ukr.net>
2026-01-14 14:30:45 +0100

    Implement IsoscelesTrapezoid

---
+++ src/main/java/core/basesyntax/figure/IsoscelesTrapezoid.java
@@ -0,0 +1,28 @@
+package core.basesyntax.figure;
+
+public class IsoscelesTrapezoid extends Figure{
+    private  final double height;
+    private final double baseA;
+    private final double  baseB;
+
+    public IsoscelesTrapezoid(String color, double height, double baseA, double baseB) {
+        super(color);
+        this.height = height;
+        this.baseA = baseA;
+        this.baseB = baseB;
+    }
+
+    @Override
+    public double getArea() {
+        return (baseA + baseB) / 2 * height;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Figure: Isosceles Trapezoid: " + getArea() + " sq. units "
+                + "height " + height
+                + " baseA " + baseA
+                + " baseB " + baseB
+                + " color " + getColor());
+    }
+}
